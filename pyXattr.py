4f1b372c4dfd8c0b1cfe75267d71066b3bc21029
Roberto <foice.news@gmail.com>
2017-11-18 21:30:26 +0100

    kik is now a class that has in it both the tags and the bibitem

--- pyXattr.py
+++ pyXattr.py
@@ -13,7 +13,7 @@ from beautifultable import BeautifulTable
 # keep track of the same in a python dictionary serialized at $KikDeskFile
 
 # TODO
-# - remove a  tag from a file
+# [] carry the BibItem in from the AppleScript and store it as a tag this would add a new argument -b which can be empty, but that will be always filled when called from BibDesk
 
 VERSION=0.3
 def DEBUG():
@@ -38,7 +38,7 @@ def serialize_dict_to_file_as_json(_emptydic,KikDeskFile):
         json.dump(_emptydic, outfile)
 
 def list_tags_in_reverse_time(current_kikdb):
-    tags_times=[ [ [ tag["tag"], tag["time"] ] for tag in tags ] for filekey, tags in current_kikdb.items() ]
+    tags_times=[ [ [ tag["tag"], tag["time"] ] for tag in get_tags_from_kik(kiks) ] for filekey, kiks in current_kikdb.items() ]
     faltlist=utils.flattenOnce(tags_times)
     return utils.sort_by_ith(faltlist,1)
 
@@ -61,7 +61,36 @@ def get_current_pyXattr(filename):
 
     return son
 
-def check_if_tag_exists(initial_tags_set,tag):
+class kikData():
+    """just a data structure to move around tags and bibitem information"""
+    def __init__(self):
+        self.tags = ""
+        self.bibitem = ""
+
+def get_tags_from_kik(kik_set):
+    result=[]
+    for element in kik_set:
+        try:
+            if len(element["tag"])>0:
+                result.append(element)
+        except KeyError:
+            if DEBUG(): print debugline, element, " was not a tag"
+    return result
+
+def get_bibitem_from_kik(kik_set):
+    result=[]
+    for element in kik_set:
+        try:
+            if len(element["bibitem"])>0:
+                result=element["bibitem"]
+        except KeyError:
+            if DEBUG(): print debugline, element, " was not a bibitem"
+    return result
+
+
+def check_if_tag_exists(initial_kik_set,tag):
+    initial_tags_set=get_tags_from_kik(initial_kik_set)
+
     res=0
     for tt in range(len(initial_tags_set)):
         if initial_tags_set[tt]["tag"] == tag:
@@ -69,7 +98,20 @@ def check_if_tag_exists(initial_tags_set,tag):
             print "tag ", tag, " already present. will not add it again"
     return res
 
-def extend_tags(initial_tags_set,tags):
+def extend_tags(initial_kik_set,kik):
+
+    initial_tags_set=get_tags_from_kik(initial_kik_set)
+    initial_bibitem=get_bibitem_from_kik(initial_kik_set)
+
+    #initial_tags_set must be split in the bibitem part and the rest
+    tags=kik.tags
+
+    bibitem=""
+    if len(kik.bibitem)>0:
+        bibitem=kik.bibitem
+    elif len(initial_bibitem)>0:
+        bibitem=initial_bibitem
+
     working_tags_set = initial_tags_set;
     mod_time=datetime.datetime.now()
     human_time = mod_time.strftime("%A, %d. %B %Y %I:%M%p")
@@ -87,9 +129,25 @@ def extend_tags(initial_tags_set,tags):
                 _this_tag["time"]=epoch_time
                 working_tags_set.append(_this_tag)
                 # add this tag to the registry ...
+    if len(bibitem)>0:
+        _this_tag={}
+        _this_tag["bibitem"]=bibitem
+        working_tags_set.append(_this_tag)
+
     return working_tags_set
 
-def remove_tags(initial_tags_set,tags):
+def remove_tags(initial_kik_set,kik):
+    initial_tags_set=get_tags_from_kik(initial_kik_set)
+    initial_bibitem=get_bibitem_from_kik(initial_kik_set)
+
+    tags=kik.tags
+
+    bibitem=""
+    if len(kik.bibitem)>0:
+        bibitem=kik.bibitem
+    elif len(initial_bibitem)>0:
+        bibitem=initial_bibitem
+
     working_tags_set = initial_tags_set;
     mod_time=datetime.datetime.now()
     human_time = mod_time.strftime("%A, %d. %B %Y %I:%M%p")
@@ -103,40 +161,58 @@ def remove_tags(initial_tags_set,tags):
             if check_if_tag_exists(initial_tags_set,clean_tag)>0:
                 print "... will remove it, instead."
                 working_tags_set = [ existing_tag for existing_tag in working_tags_set if clean_tag != existing_tag["tag"] ]
+
+    if len(bibitem)>0:
+        _this_tag={}
+        _this_tag["bibitem"]=bibitem
+        working_tags_set.append(_this_tag)
     return working_tags_set
 
+
+# TODO
+# option to use this script that implies the same operation is done on OS X tags usings
+# tag -a file
+# tag -r file
+# TODO
+# add a function to change the keyword field of the bibtex file, possibly through BibDesk, which is likely to be concurrently using the file, hence better not pass above it.
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "-t", "--add_tag", default="", help="tags to be added. CSV format")
     parser.add_argument("-r", "--remove_tag", default="", help="tags to be removed. CSV format")
     parser.add_argument('-f', '--filename', default="", help="file to be processed")
+    parser.add_argument("-b", "--bibitem", default="", help="bibitem string to be added as a tag")
     parser.add_argument('-l', '--list', default=False, action="store_true", help="list recently used tags putting most revent the bottom of the  list")
     args = parser.parse_args()
     ###################################
     filename=args.filename
-
+    listing=args.list
+    bibitem=args.bibitem
     grow=len(args.add_tag)
     shrink=len(args.remove_tag)
+    tags=""
     if grow > 0:
         tags=args.add_tag
     if shrink > 0:
         tags=args.remove_tag
 
-    listing=args.list
     ###################################
 
     if (len(filename)>0):
         #get the current tag value. it is a is a list of dictionaries
-        initial_tags_set = get_current_pyXattr(filename)
+        initial_kik_set = get_current_pyXattr(filename)
     if len(tags)>0: # is the same as saying -a or -d  was provided
+        kik=kikData()
+        kik.bibitem=bibitem
+        kik.tags=tags
 
         if grow > 0:
             # extend the tags
-            working_tags_set=extend_tags(initial_tags_set,tags)
+            working_tags_set=extend_tags(initial_kik_set,kik)
+
         if shrink > 0:
             # reduce the tags
-            working_tags_set=remove_tags(initial_tags_set,tags)
+            working_tags_set=remove_tags(initial_kik_set,kik)
 
         # make the tags into JSON
         pyXtag=json.dumps(working_tags_set)
